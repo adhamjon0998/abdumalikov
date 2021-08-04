64e5142909d11e3cb1810a323fd9eb82605f5433
adhamjon0998 <88327177+adhamjon0998@users.noreply.github.com>
2021-08-04 11:36:17 +0500

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.price').forEach(node =>{
-    node.textContent = new Intl.NumberFormat('en-En',{
-        currency: 'usd',
-        style: 'currency'
-    }).format(node.textContent)
-});
